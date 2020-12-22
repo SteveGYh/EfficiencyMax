95ba531cf308e44cd0b028426d097c31c99af97e
SteveGYh <gaoyihansteve@gmail.com>
2020-12-21 20:42:17 -0500

    File update

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+File: main.py
+Description: This is the main file to run the program.
+Program flow: One module to read/write data from/into a file and store the data in a user defined
+data structure. Another module will tansform data into UI display.
+'''
